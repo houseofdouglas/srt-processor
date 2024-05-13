578a8cff730756340042b6e1e434f9fe5966b6e6
Peter Douglas <peter@thehouseofdouglas.com>
2024-05-12 19:25:59 -0600

    Final changes for working SRT format

--- fix_srt.py
+++ fix_srt.py
@@ -16,10 +16,10 @@ class caption:
         self.text = text
 
     def __str__(self):
-        return f'{self.position}\n{self.start} --> {self.end}\n{self.text}'
+        return f'{self.position}\n{self.start} --> {self.end}\n{self.text}\n'
 
     def __repr__(self):
-        return f'{self.position}\n{self.start} --> {self.end}\n{self.text}'
+        return f'{self.position}\n{self.start} --> {self.end}\n{self.text}\n'
 
     def __add__(self, other):
         return caption(self.position, self.start, other.end, f'{self.text} {other.text}')
@@ -44,14 +44,14 @@ class caption:
     
     @property
     def duration(self):
-        return self.convert_to_seconds(self.end) - self.convert_to_seconds(self.start)
-
-    def convert_to_seconds(time):
-        hours, minutes, seconds = map(int, time.split(':'))
-        milliseconds = int(seconds.split(',')[1])
-        seconds = int(seconds.split(',')[0])
-        total_seconds = hours * 3600 + minutes * 60 + seconds + milliseconds / 1000
-        return total_seconds
+        def convert_to_seconds(time):
+            hours, minutes, seconds = time.split(':')
+            milliseconds = int(seconds.split(',')[1])
+            seconds = int(seconds.split(',')[0])
+            total_seconds = int(hours) * 3600 + int(minutes) * 60 + int(seconds) + int(milliseconds) / 1000
+            return total_seconds
+        
+        return convert_to_seconds(self.end) - convert_to_seconds(self.start)
     
     
 
@@ -64,19 +64,37 @@ def read_srt_file(file_path):
 if __name__ == '__main__':
     srt_lines = read_srt_file(FILE)
     captions = []
+    caption_obj = None
     for line in srt_lines:
         if re.match(caption_start, line):
+            if caption_obj is not None:
+                captions.append(caption_obj)
             caption_obj = caption(int(line))
         elif re.match(caption_timelime, line):
             start, end = line.split(' --> ')
-            caption_obj.set_start(start)
-            caption_obj.set_end(end)
+            caption_obj.set_start(start.strip())
+            caption_obj.set_end(end.strip())
         elif line.strip() == '':
-            captions.append(caption_obj)
+            pass
         else:
-            caption_obj.set_text(line)
+            caption_obj.set_text(line.strip())
 
-    for idx, caption in enumerate(captions):
-        if caption.duration < 1:
-            captions[idx-1] = captions[idx-1] + caption
-            captions.pop(idx)
+    if caption_obj is not None:
+        captions.append(caption_obj)
+
+    indices_to_remove = []
+    for idx, entry in enumerate(captions):
+        if entry.duration < 1 and captions[idx-1].duration < 2:
+            captions[idx-1] = captions[idx-1] + entry
+            indices_to_remove.append(idx)
+            continue
+        if entry.duration < 1 and captions[idx-1].duration > 2 and idx+1 <= len(captions)-1 and captions[idx+1].duration > 2:
+            captions[idx-1] = captions[idx-1] + entry
+            indices_to_remove.append(idx) 
+        
+    for idx in reversed(indices_to_remove):
+        captions.pop(idx)
+
+    with open('new.srt', 'w') as srt:
+        for idx, entry in enumerate(captions):
+            srt.write(f'{idx+1}\n{entry.start} --> {entry.end}\n{entry.text}\n\n')
